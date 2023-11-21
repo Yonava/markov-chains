8e80217355158010d6b339b6968093681b85200e
yonava <yona@vossandreae.com>
2023-11-21 01:01:34 -0500

    kill box

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -20,12 +20,21 @@
           Run Simulation
         </button>
 
+        <!-- node killer -->
+        <div
+          ref="killBox"
+          class="bg-red-800 absolute bottom-0 right-0 hover:bg-red-700 text-white text-3xl hover:bg-red-700 px-10 py-5"
+        >
+          Delete Node
+        </div>
+
         <!-- nodes -->
         <div
           v-for="node in nodes"
           :key="node.id"
         >
           <button
+            @mouseup="checkDeleteNode"
             class="fixed w-20 h-20 bg-gray-800 rounded-full flex items-center justify-center hover:bg-gray-900 border-4 border-gray-900"
             :style="node.style"
             :ref="(el) => (node.ref = el)"
@@ -53,29 +62,52 @@ type Node = {
   ref: Element | ComponentPublicInstance | null | any
 }
 
+const nodesCreated = ref(0)
+
 const nodes = ref<Node[]>([])
+
+const killBox = ref(null)
 
 const addNode = async () => {
   const node: Node = {
-    id: nodes.value.length + 1,
+    id: nodesCreated.value++,
     style: null,
     children: [],
     ref: null,
   }
 
   nodes.value.push(node)
+
   // wait for event loop to finish before getting node ref
   await new Promise((resolve) => setTimeout(resolve, 0))
-  console.log(node.ref)
-  // if (!nodeRef) return console.error('error creating new node')
-  const { style, x, y } = useDraggable(node.ref, {
+
+  const { style } = useDraggable(node.ref, {
     initialValue: {
       x: Math.floor(Math.random() * 1000),
       y: Math.floor(Math.random() * 1000)
     },
   })
-  // find node and update style
+
   const index = nodes.value.findIndex((n) => n.id === node.id)
   nodes.value[index].style = style
+}
+
+const checkDeleteNode = (event: any) => {
+  // see if node is in kill box
+  const killBoxRect = killBox.value.getBoundingClientRect()
+  const nodeRect = event.target.getBoundingClientRect()
+
+  console.log(killBoxRect, nodeRect)
+
+  if (
+    nodeRect.x > killBoxRect.x &&
+    nodeRect.x < killBoxRect.x + killBoxRect.width &&
+    nodeRect.y > killBoxRect.y &&
+    nodeRect.y < killBoxRect.y + killBoxRect.height
+  ) {
+    // delete node
+    const index = nodes.value.findIndex((n) => n.id === event.target.id)
+    nodes.value.splice(index, 1)
+  }
 }
 </script>
