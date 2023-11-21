9413c4f41c02bfea4fd307bcfbb1ee43dc9437d2
yonava <yona@vossandreae.com>
2023-11-21 00:48:29 -0500

    nodes are draggable

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,9 +4,10 @@
       <h1 class="text-5xl font-bold text-white mb-5">
         Create A Markov Chain
       </h1>
-      <div class="a-12 w-full h-[900px] bg-gray-700 rounded-xl relative overflow-hidden">
+      <div class="w-full h-[900px] bg-gray-700 rounded-xl relative overflow-hidden">
         <!-- control panel -->
         <button
+          @click="addNode"
           class="bg-gray-800 absolute top-0 left-0 w-60 h-20 hover:bg-gray-900 text-white text-3xl"
         >
           New Node
@@ -14,59 +15,67 @@
 
         <!-- simulate -->
         <button
-          class="bg-gray-800 absolute top-0 right-0 w-60 h-20 hover:bg-gray-900 text-white text-3xl"
+          class="bg-gray-800 absolute top-0 right-0 w-60 h-20 hover:bg-gray-900 text-white text-3xl z-10"
         >
           Run Simulation
         </button>
 
         <!-- nodes -->
-        <button
+        <div
           v-for="node in nodes"
           :key="node.id"
-          class="absolute w-20 h-20 bg-gray-800 rounded-full flex items-center justify-center hover:bg-gray-900"
-          :style="{ top: node.y + 'px', left: node.x + 'px' }"
         >
-          <span class="text-white text-3xl">{{ node.id }}</span>
-        </button>
+          <button
+            class="fixed w-20 h-20 bg-gray-800 rounded-full flex items-center justify-center hover:bg-gray-900 border-4 border-gray-900"
+            :style="node.style"
+            :ref="(el) => (node.ref = el)"
+          >
+            <span class="text-white text-3xl">
+              {{ node.id }}
+            </span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, type ComponentPublicInstance, type ComputedRef } from 'vue'
+import { useDraggable } from '@vueuse/core'
 
 type Node = {
   id: number
-  x: number
-  y: number
+  style: any
   children: number[]
+  // useDraggable throws error if HTMLElement is not passed,
+  // but vue3 throws error when not using ComponentPublicInstance... wtf!
+  ref: Element | ComponentPublicInstance | null | any
 }
 
-const nodes = ref<Node[]>([
-  {
-    id: 0,
-    x: 100,
-    y: 100,
-    children: [1, 2],
-  },
-  {
-    id: 1,
-    x: 300,
-    y: 100,
-    children: [3],
-  },
-  {
-    id: 2,
-    x: 300,
-    y: 300,
-    children: [3],
-  },
-  {
-    id: 3,
-    x: 500,
-    y: 200,
+const nodes = ref<Node[]>([])
+
+const addNode = async () => {
+  const node: Node = {
+    id: nodes.value.length + 1,
+    style: null,
     children: [],
-  },
-])
+    ref: null,
+  }
+
+  nodes.value.push(node)
+  // wait for event loop to finish before getting node ref
+  await new Promise((resolve) => setTimeout(resolve, 0))
+  console.log(node.ref)
+  // if (!nodeRef) return console.error('error creating new node')
+  const { style, x, y } = useDraggable(node.ref, {
+    initialValue: {
+      x: Math.floor(Math.random() * 1000),
+      y: Math.floor(Math.random() * 1000)
+    },
+  })
+  // find node and update style
+  const index = nodes.value.findIndex((n) => n.id === node.id)
+  nodes.value[index].style = style
+}
 </script>
