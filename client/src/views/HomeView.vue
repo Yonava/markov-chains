22e17bf262aae331decdbd3d438cd613b4f6a755
yonava <yona@vossandreae.com>
2023-11-20 23:29:21 -0500

    main view

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,12 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="absolute w-full h-full bg-gray-600">
+    <div class="flex flex-col items-center justify-center h-full p-12">
+      <h1 class="text-5xl font-bold text-white mb-5">
+        Create A Markov Chain
+      </h1>
+      <div class="a-12 w-full h-[700px] bg-gray-700 rounded-xl">
+
+      </div>
+    </div>
+  </div>
 </template>
