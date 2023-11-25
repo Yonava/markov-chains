7de38ccca5e73c3ca5661921e2da472f5161592c
yonava <yona@vossandreae.com>
2023-11-25 00:57:19 -0500

    actually working steady state with big number

--- client/src/useLinearAlgebra.ts
+++ client/src/useLinearAlgebra.ts
@@ -1,16 +1,63 @@
 // @ts-ignore
-import rref from 'rref';
-// @ts-ignore
 import linearAlgebra from 'linear-algebra';
 import { computed, type Ref } from 'vue';
 
-const { Matrix, Vector } = linearAlgebra();
+import BigNumber from 'bignumber.js';
 
-const matrix = [
-  [1, 2, -1, -4],
-  [2, 3, -1, -11],
-  [-2, 0, -3, 22],
-];
+const { Matrix } = linearAlgebra();
+
+// reduces augmented matrix to reduced row echelon form in place
+const findRREF = (matrix: BigNumber[][]) => {
+
+  const numRows = matrix.length;
+  const numCols = matrix[0].length;
+
+  let lead = 0;
+
+  for (let row = 0; row < numRows; row++) {
+    if (numCols <= lead) {
+      return matrix;
+    }
+
+    let i = row;
+
+    while (matrix[i][lead].eq(0)) {
+      i++;
+      if (numRows === i) {
+        i = row;
+        lead++;
+        if (numCols === lead) {
+          return matrix;
+        }
+      }
+    }
+
+    // Swap rows
+    let temp = matrix[i];
+    matrix[i] = matrix[row];
+    matrix[row] = temp;
+
+    // Scale the pivot row
+    let val = matrix[row][lead];
+    for (let j = 0; j < numCols; j++) {
+      matrix[row][j] = matrix[row][j].div(val);
+    }
+
+    // Eliminate other rows
+    for (let i = 0; i < numRows; i++) {
+      if (i !== row) {
+        val = matrix[i][lead];
+        for (let j = 0; j < numCols; j++) {
+          matrix[i][j] = matrix[i][j].minus(val.times(matrix[row][j]));
+        }
+      }
+    }
+
+    lead++;
+  }
+
+  return matrix;
+}
 
 export function useSteadyStateAnalysis(inputTransitionMatrix: Ref<number[][]>) {
   return computed(() => {
@@ -20,6 +67,11 @@ export function useSteadyStateAnalysis(inputTransitionMatrix: Ref<number[][]>) {
       [0.4, 0.3, 0.3],
       [0.3, 0.3, 0.4],
     ]
+
+    // const test = [
+    //   [0.01, 0.99],
+    //   [0.01, 0.98]
+    // ]
 
     if (test.length < 2) {
       return {
@@ -43,24 +95,30 @@ export function useSteadyStateAnalysis(inputTransitionMatrix: Ref<number[][]>) {
     const newArray = new Array(augmentedMatrix[0].length).fill(1);
     augmentedMatrix.push(newArray);
 
-    // get rref of augmented matrix
-    // const rrefMatrix = getRREF(augmentedMatrix);
+    // map over everything in augmented matrix and convert toFixed(2)
 
-    // console.log(rrefMatrix);
+    const matrixData = augmentedMatrix.map((row: number[]) => {
+      return row.map((value: number) => {
+        return Number(value.toFixed(2));
+      })
+    })
 
-    // steady state vector
-    return {
-      augmentedMatrix,
-      matrixData: test,
-      rrefMatrix: [],
-    };
+    const bigNumberMatrix = matrixData.map((row: number[]) => {
+      return row.map((value: number) => {
+        return new BigNumber(value);
+      })
+    });
+
+    // find rref of augmented matrix
+    const sol = findRREF(bigNumberMatrix);
+
+    // convert solution back into numbers
+    sol.map((row: BigNumber[]) => {
+      return row.map((value: BigNumber) => {
+        return value.toNumber();
+      })
+    });
+
+    return new Matrix(sol).trans().data.at(-1);
   })
-}
-
-// takes augmented matrix and returns reduced row echelon form
-const getRREF = (matrix: number[][]): number[][] => {
-  const newMatrix = JSON.parse(JSON.stringify(matrix));
-  const reduced = rref(newMatrix);
-  console.log(reduced);
-  return reduced;
 }
